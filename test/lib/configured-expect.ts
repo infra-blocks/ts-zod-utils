6cc9e6f2b10059aae98c27b7883fe143bbf71e30
PhilLavoie <PhilLavoie@users.noreply.github.com>
2023-06-28 08:31:30 -0400

    Use @infra-blocks/test

--- test/lib/configured-expect.ts
+++
@@ -1,8 +0,0 @@
-import * as chai from "chai";
-import chaiPromise from "chai-as-promised";
-import sinonChai from "sinon-chai";
-
-chai.use(chaiPromise);
-chai.use(sinonChai);
-
-export const expect = chai.expect;
