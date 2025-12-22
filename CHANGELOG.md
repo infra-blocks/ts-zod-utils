1af0da7c234e2122a17bbd20c846910a03e14279
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-12-22 13:26:47 -0500

    csv

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [0.5.0] - 2025-12-22
 
+### Added
+
+- The `zu.csv()` schema to parse and transform comma separated strings into arrays.
+
 ### Changed
 
 - Updated to `zod@4`. This exports the breaking changes between `zod@3` and `zod@4`.
--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,7 +1,26 @@
+import { expect } from "@infra-blocks/test";
+import { zu } from "../../src/index.js";
 import { injectGeoJsonTests } from "./geojson/index.js";
 import { injectJsonTests } from "./json/index.js";
 
 describe("zu", function () {
   injectGeoJsonTests();
   injectJsonTests();
+  describe(zu.csv.name, function () {
+    it("should throw with undefined", function () {
+      expect(() => zu.csv().parse(undefined)).to.throw();
+    });
+    it("should resolve to an array with empty string with an empty string", function () {
+      expect(zu.csv().parse("")).to.deep.equal([""]);
+    });
+    it("should split a comma-separated string", function () {
+      expect(zu.csv().parse("a,b,c")).to.deep.equal(["a", "b", "c"]);
+    });
+    it("should work otherwise with a default value", function () {
+      expect(zu.csv().default(["x", "y"]).parse(undefined)).to.deep.equal([
+        "x",
+        "y",
+      ]);
+    });
+  });
 });
