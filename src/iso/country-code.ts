8f2a02ce3b15d2676e096d9c60203c605cafa551
PhilLavoie <PhilLavoie@users.noreply.github.com>
2026-01-17 07:07:05 -0500

    correctly document & test iso.countryCode.alpha3

--- src/iso/country-code.ts
+++ src/iso/country-code.ts
@@ -7,18 +7,18 @@ const alpha3CodeSchema = z.enum(ALPHA_3_CODES).brand("IsoAlpha3CountryCode");
 
 export type IsoAlpha3CountryCode = z.infer<typeof alpha3CodeSchema>;
 
-/**
- * Returns a zod schema that validates that the input is a string that matches
- * an ISO 3166 alpha 3 country code.
- *
- * The currency code is case-sensitive and all current currency codes are
- * all uppercase.
- *
- * @returns A zod schema that validates ISO alpha 3 code strings and return the {@link IsoAlpha3CountryCode}
- * branded type as output.
- *
- * @see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
- */
 export const countryCode = {
+  /**
+   * Returns a zod schema that validates that the input is a string that matches
+   * an ISO 3166 alpha 3 country code.
+   *
+   * The currency code is case-sensitive and all current currency codes are
+   * all uppercase.
+   *
+   * @returns A zod schema that validates ISO alpha 3 code strings and return the {@link IsoAlpha3CountryCode}
+   * branded type as output.
+   *
+   * @see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+   */
   alpha3: () => alpha3CodeSchema,
 };
